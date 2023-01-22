a78995abefdc009780b67a9db76c00fe9398d27c
unknown <iury.flores@gmail.com>
2023-01-22 09:11:56 -0300

    Adding cors

--- app.js
+++ app.js
@@ -1,57 +1,60 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
+import express from "express";
+import * as dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-import './db/index.js'
+import "./db/index.js";
 
-const app = express()
+import cors from "cors";
 
-app.use(express.json())
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('It works')
-})
+app.use(cors());
+app.use(express.json());
 
-import Medium from './models/Medium.model.js'
+app.get("/", (req, res) => {
+  res.send("It works");
+});
 
-app.post('/medium', async (req, res) => {
-  const { body } = req
+import Medium from "./models/Medium.model.js";
+
+app.post("/medium", async (req, res) => {
+  const { body } = req;
   try {
-    const newMedium = await Medium.create(body)
-    res.status(201).json(newMedium)
+    const newMedium = await Medium.create(body);
+    res.status(201).json(newMedium);
   } catch (error) {
-    res.status(400).json({ status: 400, msg: error.message })
+    res.status(400).json({ status: 400, msg: error.message });
   }
-})
+});
 
-app.get('/medium', async (req, res) => {
+app.get("/medium", async (req, res) => {
   try {
-    const media = await Medium.find()
-    res.status(200).json(media)
+    const media = await Medium.find();
+    res.status(200).json(media);
   } catch (error) {
-    res.status(500).json({ status: 500, msg: error.message })
+    res.status(500).json({ status: 500, msg: error.message });
   }
-})
+});
 
-app.get('/medium/:id', async (req, res) => {
-  const { id } = req.params
+app.get("/medium/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    const medium = await Medium.findById(id)
+    const medium = await Medium.findById(id);
     if (medium) {
-      res.status(200).json(medium)
+      res.status(200).json(medium);
     } else {
-      res.status(404).json({ msg: 'Title not found' })
+      res.status(404).json({ msg: "Title not found" });
     }
   } catch (error) {
-    if (error.name === 'CastError') {
-      res.status(404).json({ status: 404, msg: error })
+    if (error.name === "CastError") {
+      res.status(404).json({ status: 404, msg: error });
     } else {
-      res.status(500).json({ status: 500, msg: error.message })
+      res.status(500).json({ status: 500, msg: error.message });
     }
   }
-})
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT} port`)
-})
+  console.log(`Server running on http://localhost:${process.env.PORT} port`);
+});
